5e3842b26b3c0679bc8df7513735423f355b8be0
sara <109745554+sara19git@users.noreply.github.com>
2026-04-02 20:38:54 +0100

    Refactor variable declarations and update event listeners

--- script.js
+++ script.js
@@ -1,121 +1,86 @@
-let options = ["Rock", "Paper", "Scissors"];
+const options = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
-let playerScoreElement = document.getElementById('player-score');
-let computerScoreElement = document.getElementById('computer-score');
-let roundResultsMsg = document.getElementById('results-msg');
-// we will use this buttons to test the function showResults()
-let rockBtn = document.getElementById('rock-btn');
-let paperBtn = document.getElementById('paper-btn');
-let scissorsBtn = document.getElementById('scissors-btn');
-let winnerMsg = document.getElementById('winner-msg');
-let optionsContainer = document.getElementById('options-container');
-let resetGameBtn = document.getElementById('reset-game-btn');
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
 
-let userResult = document.querySelector('.user-result img');
-let computerResult = document.querySelector('.computer-result img');
-let chooseOption = document.querySelector('#options-container h2');
-let optionImages = document.querySelectorAll('.option-image');
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
 
-optionImages.forEach((image,index) => {
-  image.addEventListener('click', (e) => {
-    image.classList.add('active');
 
-    optionImages.forEach((image2,index2) => {
-    //if the current index (index) doesn't match the clicked index 
-    // (index2) remove the (.active) class from the second option 
-     index !== index2 && image2.classList.remove("active");
-    });
-
-    
-    // get the src of the clicked image
-    let imageSrc = e.target.querySelector("img").src;
-    userResult.src = imageSrc;
-
-    // generate a random computer choice between (rock, paper and scissor)
-    let randomComputerChoice = getRandomComputerResult();
-    console.log("image choice: " +randomComputerChoice);
-    let ComputerIndex = options.indexOf(randomComputerChoice)
-    let computerImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
-    computerResult.src = computerImages[ComputerIndex];
-  });
-  
-})
-
- 
-// generate a random choice for the computer
-function getRandomComputerResult(){
-    let randomChoice = Math.floor(Math.random() * options.length);
-    return options[randomChoice];
+function getRandomComputerResult() {
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
 
-// determining if the player won the round
-function hasPlayerWonTheRound(playerChoice, computerChoice){
-  if(playerChoice === computerChoice){
-    return false;
-  } else if(playerChoice === "Rock" && computerChoice === "Scissors" || 
-            playerChoice === "Scissors" && computerChoice === "Paper" ||
-            playerChoice === "Paper" && computerChoice === "Rock"
-  ){
-    return true;
+
+function hasPlayerWonTheRound(playerChoice, computerChoice) {
+  return (
+    (playerChoice === "Rock" && computerChoice === "Scissors") ||
+    (playerChoice === "Scissors" && computerChoice === "Paper") ||
+    (playerChoice === "Paper" && computerChoice === "Rock")
+  );
+}
+
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
   } else {
-    return false;
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
   }
 }
 
-// determinig the results of the round
-function getRoundResults(userOption){
-    const computerResult = getRandomComputerResult();
-    console.log("computer choice: " +computerResult)
-    if(userOption === computerResult){  
-        return "it's a tie both chose: " + userOption;
-    }
-    else if(hasPlayerWonTheRound(userOption, computerResult)){
-        playerScore++;
-        return "the player wins: " + userOption + " beats " + computerResult;
-    } else {
-        computerScore++;
-        return "computer wins: " + computerResult + " beats " + userOption;
-    }
-}
 
-// create a functinality that will show the results
-function showResults(userOption, randomComputerChoice){
-  roundResultsMsg.innerText = getRoundResults(userOption, randomComputerChoice);
-  playerScoreElement.innerText = playerScore;
-  computerScoreElement.innerText = computerScore;  
-  if(playerScore === 3){
-    winnerMsg.innerText = "Player has won the game!";
-    resetGameBtn.style.display = "block";
-    optionsContainer.style.display = "none";
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
 
-  } else if(computerScore === 3){
-    winnerMsg.innerText = "computer has won the game!";
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${
+      playerScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+
     resetGameBtn.style.display = "block";
     optionsContainer.style.display = "none";
   }
-  
-}
+};
 
-rockBtn.addEventListener('click', function(){
+
+rockBtn.addEventListener("click", function () {
   showResults("Rock");
 });
-paperBtn.addEventListener('click', function(){
+
+paperBtn.addEventListener("click", function () {
   showResults("Paper");
 });
-scissorsBtn.addEventListener('click', function(){
+
+scissorsBtn.addEventListener("click", function () {
   showResults("Scissors");
 });
 
-resetGameBtn.addEventListener('click', resetGame);
+resetGameBtn.addEventListener("click", resetGame);
 
 function resetGame(){
   playerScore = 0;
   computerScore = 0;
-  playerScoreElement.innerText = playerScore;
-  computerScoreElement.innerText = computerScore;
-  resetGameBtn.style.display = 'none';
-  optionsContainer.style.display = 'block';
-  winnerMsg.innerText = "";
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+  resetGameBtn.style.display = "none";
+  optionsContainer.style.display = "block";
+  winnerMsgElement.innerText = "";
   roundResultsMsg.innerText = "";
+
 }
